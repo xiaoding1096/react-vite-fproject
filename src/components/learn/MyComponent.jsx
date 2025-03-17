c281d762fceee8e7478fdc9fa453770e0eab5cd6
xiaoding1096 <hoangnguyendt1096@gmail.com>
2025-03-17 21:22:29 +0100

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,17 @@
+//JSX
+//fragment
+import './style.css';
+
 const MyComponent = () => {
   return (
-    <div> Nguyen </div>
+    <>
+       <div> Nguyen </div>
+       <div className="child"
+            style={
+                {borderRadius: "10px"}
+            }
+            > Dink </div>
+    </>
   );
 }
 
