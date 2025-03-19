6837164c4e12a5d72b75e3f6f32e1c7386d6d8b0
xiaoding1096 <hoangnguyendt1096@gmail.com>
2025-03-19 21:08:53 +0100

    video 36

--- src/components/todo/TodoAdd.jsx
+++ src/components/todo/TodoAdd.jsx
@@ -1,10 +1,17 @@
 const TodoAdd = (props) => {
     const {addNewTodo} = props;
     // addNewTodo("The Money");
+    const handleClick = () => {
+        alert("click me");
+    }
+
+    const handleOnChange = (name) => {
+        console.log(name);
+    }
     return (
         <div className="todo-add">
-            <input type="text" />
-            <button type="button">Add</button>
+            <input type="text" onChange = {(event) => {handleOnChange(event.target.value)}}/>
+            <button type="button" onClick={handleClick}>Add</button>
         </div>
     )
 }
