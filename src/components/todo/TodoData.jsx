3425a44caf25d455028fbb2266835f727957461b
xiaoding1096 <hoangnguyendt1096@gmail.com>
2025-03-19 18:18:29 +0100

    video 34 plus

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,10 +1,11 @@
 const TodoData = (props) => {
     console.log(">> check props :", props);
-    const {name, age} = props ;
+    const {name, age, data} = props ;
     return (
         <div className="todo-data">
             <div>My name is {name}</div>
             <div>I'm {age}</div>
+            <div>This is my address {data.address} and this is my zipcode {data.zip}</div>
             <div>Learning React</div>
             <div>Watching Youtube</div>
         </div>
