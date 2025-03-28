cfed4388a2b7e690468f9614137990e3078c8ecc
xiaoding1096 <hoangnguyendt1096@gmail.com>
2025-03-28 20:17:03 +0100

    video 44

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,7 @@ import {useState} from "react";
 
 
 const App = () => {
-  const [todoList, setTodoList] = useState([
-    // {id: 1, name: "Learning React"},
-    // {id: 2, name: "Watching Youtube"},
-    // {id: 3, name: "Watching Youtube"},
-  ])
+  const [todoList, setTodoList] = useState([])
   const addNewTodo = (name) => {
     const newTodo = {
       id: randomIntFromInterval(1,1000000),
@@ -18,10 +14,11 @@ const App = () => {
     }
     setTodoList([...todoList, newTodo]);
   }
+  
   const deleteTodo = (id) => {
     const newTodo = todoList.filter(item => item.id !== id);
     setTodoList(newTodo);
-  }
+        }
 
   const randomIntFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -30,7 +27,9 @@ const App = () => {
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoAdd addNewTodo = {addNewTodo}/>
-      {todoList.length > 0 && <TodoData
+      {todoList.length > 0 && 
+      
+      <TodoData
       todoList = {todoList}
       deleteTodo = {deleteTodo}
       />
